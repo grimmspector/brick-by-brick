ada8a4dbeb2289a0d39c0a3cfe035b924d387f66
grimmspector <65185497+grimmspector@users.noreply.github.com>
2026-04-08 18:36:00 -0400

    File renames for CS Issues

--- brickbybrick/brickbybrickModSystem.cs
+++ brickbybrick/brickbybrickModSystem.cs
@@ -1,17 +1,19 @@
-﻿using Vintagestory.API.Client;
+﻿using brickbybrick.Blocks;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-namespace brick_by_brick
+namespace brickbybrick
 {
-    public class brick_by_brickModSystem : ModSystem
+    public class brickbybrickModSystem : ModSystem
     {
         // Called on server and client
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
             Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            api.RegisterBlockClass(Mod.Info.ModID + ".trampoline", typeof(BlockTrampoline));
         }
 
         public override void StartServerSide(ICoreServerAPI api)
